8b127ac89e95c5d053b674f7c177e7d6da188b96
mazen shabaan <mazenshabaan41@gmail.com>
2025-07-20 18:59:28 +0300

    first commit

---
+++ lib/presentation/features/home/view/home_screen.dart
@@ -0,0 +1,23 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:session/core/app_assets.dart';
+
+class HomeScreen extends StatefulWidget {
+  HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  final TextEditingController field = TextEditingController();
+  bool isValid = false;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(body: Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+      
+        ],
+      ));
+  }
+}
